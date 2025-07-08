9b10eaae232e308d258cb1a21ba9ebd7bc2f3768
khw7876 <khw7876@xingxingmobility.com>
2025-07-08 17:41:32 +0900

    [test/test 웹훅 push 이벤트 줄 추가 및 제거 테스트2]

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -2,10 +2,6 @@ console.log('1줄');
 console.log('2줄');
 console.log('3줄');
 console.log('3줄');
-console.log('3줄');
-console.log('4줄');
-console.log('52줄');
-
 console.log('42줄');
 
 console.log('2줄 추가');
