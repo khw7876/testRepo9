f1231f11de19404aa9c109e1e6d6ddb987af429b
khw7876 <khw7876@xingxingmobility.com>
2025-08-01 16:10:26 +0900

    [test]

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1129,6 +1129,9 @@ console.log('test');
 console.log('test');
 console.log('test');
 console.log('test');
+console.log('test');
+console.log('test');
+console.log('test');
 
 // 80줄 + 480줄 = 560줄
 // 가중치 = 472.5;
